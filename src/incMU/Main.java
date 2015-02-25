349e60e2eb5ba9839514c836b93c8c01136a4842
AndrewLeady <leadyaj@gmail.com>
2015-02-24 17:37:02 -0800

    Version 1
    ex nihilo

---
+++ src/incMU/Main.java
@@ -0,0 +1,10 @@
+package incMU;
+
+import java.sql.*;
+import java.net.Socket;
+import java.net.ServerSocket;
+
+public class Main {
+	Connection database = DriveManager.getConnection(/*magic goes here*/);
+	//TODO: voodoo
+}
